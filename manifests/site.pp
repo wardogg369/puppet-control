725632be8c72a4f8a2567c86215401e8e258dcdf
root <root@jabreu081.mylabserver.com>
2018-04-10 20:15:18 +0000

    remove regex

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
-node 'jabreu085.mylabserver' {
+node 'jabreu086.mylabserver' {
   notify { 'This matches the node name!!!!': }
 }
 
-node /^jabreu085/ {
-  notify { "This is a node definition using regex!!!!": }
-}
+#node /^jabreu086/ {
+#  notify { "This is a node definition using regex!!!!": }
+#}
 
 node 'jabreu081.mylabserver.com' {
   include pe_repo::platform::el_6_x86_64
